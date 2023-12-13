cc190bc918299c00d3e2bd458a19c15573903186
Igor <iygorsilva@gmail.com>
2023-12-13 14:27:15 -0300

    Add new components and routes fot host-app

--- projects/host-app/src/app/app.component.ts
+++ projects/host-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
